11d17a96fe873cd6f8fe3b284c381fb7c6f7b6b5
Kaushik <kaushik13k@Apples-MacBook-Air.local>
2024-10-09 01:45:18 +0530

    modified the entry page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 const messages = [
-  "Hello",
-  "Namaste",
-  "Hola",
-  "Bonjour",
-  "Welcome to my Profile! ...",
+  "Hello there!",
+  "Namaste!",
+  "¡Hola!",
+  "Bonjour!",
+  "Step into my profile",
 ];
 
 const App = () => {
@@ -24,35 +26,35 @@ const App = () => {
         }
         return prevIndex + 1;
       });
-    }, 1000);
+    }, 1500);
 
     return () => clearInterval(timer);
   }, [isFinished]);
 
-  // const handleTransition = () => {
-  //   // Handle button click logic here
-  // };
-
   return (
-    <div className="bg-black text-white font-salsa font-bold flex items-center justify-center h-screen m-0">
-      <div className="text-container relative w-1/2 flex items-center justify-center text-2xl">
+    <div className="bg-[#EFF0F3] text-gray-800 font-poppins flex items-center justify-center h-screen m-0 font-bold">
+      <div className="text-container relative w-3/4 md:w-1/2 flex flex-col items-center justify-center text-xl md:text-2xl">
         {messages.map((message, index) => (
           <p
             key={index}
             className={`absolute transition-opacity duration-1000 ${
               index === currentIndex ? "opacity-100" : "opacity-0"
-            }`}
+            } text-center`}
           >
             {message}
+            {index === messages.length - 1 && isFinished && (
+              <div className="flex items-center justify-center mt-4">
+                <FontAwesomeIcon
+                  icon={faCircleArrowRight}
+                  className="ml-2 text-4xl text-gray-800 hover:text-gray-500 transition duration-300"
+                />
+                <span className="ml-2 text-gray-400 text-sm font-bold">
+                  Click here!
+                </span>
+              </div>
+            )}
           </p>
         ))}
-        {/* <button 
-                    className={`mt-5 px-12 py-3 bg-white text-black border-none cursor-pointer text-lg rounded-2xl hover:scale-105 transition-transform duration-200`} 
-                    onClick={handleTransition}
-                    disabled={!isFinished} // Disable the button if not finished
-                >
-                    Lets Gooo ...
-                </button> */}
       </div>
     </div>
   );
