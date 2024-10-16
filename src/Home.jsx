07b8bd0214ab9048556ebf8fad55f55af382c55e
Kaushik <kaushik13k@Apples-MacBook-Air.local>
2024-10-16 21:29:34 +0530

    added projects

---
+++ src/Home.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./sections/Navbar";
+import Projects from "./sections/Projects";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <Projects />
+    </div>
+  );
+};
+
+export default Home;
