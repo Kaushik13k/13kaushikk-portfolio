265cb085f624d625188a56402aee857397c19d23
Kaushik <kaushik13k@Apples-MacBook-Air.local>
2024-10-21 21:46:24 +0530

    added the code for footer

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Footer from "./sections/Footer";
 import Navbar from "./sections/Navbar";
 import Projects from "./sections/Projects";
 
@@ -7,6 +8,7 @@ const Home = () => {
     <div>
       <Navbar />
       <Projects />
+      <Footer />
     </div>
   );
 };
---
+++ src/sections/Footer.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="text-[#676451] mt-4 border-t border-[#ECE9D5] p-4 text-center fixed bottom-0 left-0 right-0 m-">
+      <p className="text-sm">
+        <span className="font-semibold">
+          © 2024 Kaushik K. All rights reserved. &nbsp;
+        </span>
+        Built with TypeScript, Tailwind CSS, and Next.js.
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
