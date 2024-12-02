67f0a632f3ddb00e322891c77b5a8fae92b9e62a
Kaushik <kaushik13k@Apples-MacBook-Air.local>
2024-12-02 23:25:52 +0530

    home page blog card in blog page nav

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -4,14 +4,15 @@ import { CldImage } from "next-cloudinary";
 import Navbar from "@app/home/sections/Navbar";
 import Footer from "@app/home/sections/Footer";
 import { PortfolioBlogs } from "@app/models/blogs";
-import { getHostIcon } from "@app/home/sections/Blogs";
 import React, { useEffect, useState } from "react";
+import { getHostIcon } from "@app/home/sections/Blogs";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import {
   faChevronLeft,
   faChevronRight,
 } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
 
 const itemsPerPage = 6;
 
@@ -63,42 +64,46 @@ function App() {
         <h1 className="flex items-center justify-center mb-10 text-5xl font-extrabold text-[#676451]">
           Blogs
         </h1>
+
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
           {currentData.map((card, index) => (
-            <div key={index} className="w-full">
-              {card.blogImage && (
-                <CldImage
-                  width="200"
-                  height="200"
-                  src={card.blogImage}
-                  alt={card.blogTitle}
-                  className="w-full h-40 sm:h-48 lg:h-52 object-cover rounded-lg"
-                  priority
-                />
-              )}
+            <Link
+              key={index}
+              href={card.hostLink}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div key={index} className="w-full">
+                {card.blogImage && (
+                  <CldImage
+                    width="200"
+                    height="200"
+                    src={card.blogImage}
+                    alt={card.blogTitle}
+                    className="w-full h-40 sm:h-48 lg:h-52 object-cover rounded-lg"
+                    priority
+                  />
+                )}
 
-              <div className="mt-4">
-                <div className="flex space-x-2 text-gray-500 text-xs">
-                  <span className="bg-gray-100 px-2 py-1 rounded-full">
-                    {card.publishDate}
-                  </span>
-                  <span className="bg-gray-100 px-2 py-1 rounded-full">
-                    {card.avgReadTime}
-                  </span>
-                  <span className="bg-gray-100 px-2 py-1 rounded-full">
-                    {getHostIcon(card.hostSource)}
-                    {/* <FontAwesomeIcon
-                      icon={faMedium}
-                      className="text-gray-700"
-                    /> */}
-                  </span>
+                <div className="mt-4">
+                  <div className="flex space-x-2 text-gray-500 text-xs">
+                    <span className="bg-gray-100 px-2 py-1 rounded-full">
+                      {card.publishDate}
+                    </span>
+                    <span className="bg-gray-100 px-2 py-1 rounded-full">
+                      {card.avgReadTime}
+                    </span>
+                    <span className="bg-gray-100 px-2 py-1 rounded-full">
+                      {getHostIcon(card.hostSource)}
+                    </span>
+                  </div>
+                  <h3 className="font-bold text-lg mt-1">{card.avgReadTime}</h3>
+                  <p className="text-sm text-gray-700 mt-2">
+                    {card.blogDescription}
+                  </p>
                 </div>
-                <h3 className="font-bold text-lg mt-1">{card.avgReadTime}</h3>
-                <p className="text-sm text-gray-700 mt-2">
-                  {card.blogDescription}
-                </p>
               </div>
-            </div>
+            </Link>
           ))}
         </div>
 
